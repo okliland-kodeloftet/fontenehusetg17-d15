3004de9fda2e82ab90e2f01ceb092cf0bc1843fb
okliland-kodeloftet <okliland@hotmail.com>
2026-04-09 11:36:07 +0200

    d

--- script.js
+++ script.js
@@ -4,10 +4,63 @@ const hamburgerIcon = document.querySelector("#hamburgerIcon");
 console.log(hamburgerIcon);
 
 hamburgerIcon.addEventListener("click", function () {
-  console.log("Noe er klikket på");
+  console.log("Hamburgerikon er klikket på");
 
   const navList = document.querySelector("#navList");
   console.log(navList);
 
   navList.classList.toggle("navHamburgerList");
 });
+
+// 
+// 
+
+
+const showMore = document.querySelector("#showMore");
+console.log(showMore);
+
+showMore.addEventListener("click", function (){
+  console.log("knappen er trykket på");
+
+  // DOM manipulasjon
+  const popUpWindow = document.createElement("div");
+  // Inline styling, som vi må kunne skrive uten hjelp fra CSS.
+  popUpWindow.style.border = "2px solid red";
+  // ClassList gjør at vi kan lage ett klassenavn som vi kan bruke inne i CSS
+  popUpWindow.classList.add("popUpBorder");
+
+  const popUpText = document.createElement("p")
+  popUpText.textContent = "Lorem ipsum dolar ect... Lorem ipsum dolar ect... Lorem ipsum dolar ect... Lorem ipsum dolar ect... Lorem ipsum dolar ect...";
+
+  popUpWindow.appendChild(popUpText);
+
+  const heroIntroContainer = document.querySelector("#heroIntroContainer");
+  console.log(heroIntroContainer);
+
+  heroIntroContainer.appendChild(popUpWindow);
+})
+
+
+// 
+// 
+
+
+function showMoreToggle(event) {
+
+
+  console.log(aboutShowMore, projectsShowMore);
+
+  const popUpText = document.createElement("p");
+  console.log(popUpText)
+  popUpText.textContent = "Trallalalalalalallaallalalalalaallaalllaalalal"
+
+  event.target.appendChild(popUpText)
+}
+
+// showMoreToggle();
+
+const aboutShowMore = document.querySelector("#aboutShowMore")
+const ProjectsShowMore = document.querySelector("#projectsShowMore")
+
+aboutShowMore.addEventListener("click", showMoreToggle)
+ProjectsShowMore.addEventListener("click", showMoreToggle)
